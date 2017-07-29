b33af6ecb9e7ea318aa78f752bf543d1d71bd798
glakshmi_Rubhu <glakshmi494@gmail.com>
2017-07-29 10:55:34 +0530

    srividya

---
+++ app/courses/srividyaone.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { IProduct } from './.models/users';
+
+@Component({
+    templateUrl: './app/products/product-edit-info.component.html'
+})
+export class ProductEditInfoComponent implements OnInit {
+    @ViewChild(NgForm) productForm: NgForm;
+
+    errorMessage: string;
+    //product = { id: 1, productName: 'test', productCode: 'test' };
+    product: IProduct;
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+
+        this.route.parent.data.subscribe(data => {
+            this.product = data['product'];
+
+            if (this.productForm) {
+                this.productForm.reset();
+            }
+        });
+
+    }
+}
